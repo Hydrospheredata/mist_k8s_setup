8f0c066fd2e3bf6717642b37dac3c328b44bf734
dos65 <qtankle@gmail.com>
2018-09-09 19:06:46 +0300

    docker build script

---
+++ Dockerfiles/README.md
@@ -0,0 +1,1 @@
+Usage: `build.sh dos65 mist-k8s 1.0.0`
---
+++ Dockerfiles/build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+SRC_DIR="mist"
+if [ ! -d $SRC_DIR ]; then
+  echo "Checkout and bulding mist"
+  git clone git@github.com:Hydrospheredata/mist.git mist
+  cd mist
+  sbt basicStage
+fi
+
+ORG=$1
+NAME=$2
+TAG=$3
+
+if [ -z $ORG ]; then
+  echo "It's required to provide organization name for docker image"
+  exit 1
+fi
+
+if [ -z $NAME ]; then
+  echo "It's required to provide image name for docker image"
+  exit 1
+fi
+
+if [ -z $TAG ]; then
+  echo "It's required to provide tag for docker image"
+  exit 1
+fi
+
+echo "Building image: $ORG/$NAME:$TAG"
+docker build -t "$ORG/$NAME:$TAG" . -f Dockerfile_mist
